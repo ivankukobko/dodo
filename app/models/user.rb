05ad08eb0f2dd878287c7779537927b903ae45a5
ivan <johnny.da.kid@gmail.com>
2011-12-25 02:53:39 +0200

    updated favicon

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   has_many :invitations, :class_name => 'Collaborator', :conditions => [ 'collaborators.accepted_at is null' ]
   has_many :projects, :through => :collaborators
   has_many :todo_items, :through => :todo_lists
+  #has_many :shared_todo_items, :through => :projects
 
   has_many :co_workers, :through => :projects, :source => :users, :uniq => true
 
