e5f2a72f2fbfdc4f880d1cfd6e36bca491ebbd8e
Ivan Kukobko <johnny.da.kid@gmail.com>
2012-01-17 22:17:44 +0200

    finished worklogs

--- app/controllers/worklogs_controller.rb
+++ app/controllers/worklogs_controller.rb
@@ -9,23 +9,38 @@ class WorklogsController < ApplicationController
   end
 
   def create
-
+    if worklog.save
+      respond_to do |format|
+        format.html { redirect_to [worklog.todo_item.todo_list, worklog.todo_item] }
+        format.json { render :json => worklog }
+      end
+    end
   end
 
   # get worklogs for current_project or
   def worklogs
-    @worklogs ||= Worklog.all
-    #if todo_item
-      #todo_item.worklogs
-    #else
+    @worklogs ||= if todo_item
+      todo_item.worklogs
+    else
       #current_user.worklogs
-    #end
+      Worklog.all
+    end
   end
   helper_method :worklogs
 
   def todo_item
     @todo_item ||= if params[:todo_item_id]
       TodoItem.find params[:todo_item_id]
+    end
+  end
+
+  def worklog
+    @worklog ||= if params[:id]
+      current_user.worklogs.find params[:id]
+    elsif todo_item
+      params[:worklog][:user_id] = current_user.id
+      params[:worklog][:log_date] = Time.now
+      todo_item.worklogs.build params[:worklog]
     end
   end
 
--- app/models/worklog.rb
+++ app/models/worklog.rb
@@ -2,6 +2,10 @@ class Worklog < ActiveRecord::Base
   belongs_to :user
   belongs_to :todo_item
 
+  validates_presence_of :log_date, :duration, :user_id, :todo_item_id
+
+  default_scope order('created_at DESC')
+
   #TODO: normalize incoming work duration
   # def duration= value
   #
