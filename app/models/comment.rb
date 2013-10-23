72ccf40480b866d3cff660ec2ddd9b1671647228
Ivan Kukobko <johnny.da.kid@gmail.com>
2013-10-24 00:46:10 +0300

    More caching: cache tasks templates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-  belongs_to :todo_item
-  belongs_to :parent, :class_name => 'Comment'
-
-  #has_paper_trail
-
+  belongs_to :todo_item, touch: true
+  belongs_to :parent, class_name: 'Comment'
 end
